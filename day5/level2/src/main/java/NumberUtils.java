ad53d3476eb472509c0f311f2164f3d9c7e8d198
harinikasturi <harinikasturi2003@gmail.com>
2025-05-23 12:09:30 +0530

    deleted file

--- day5/level2/src/main/java/NumberUtils.java
+++
@@ -1,6 +0,0 @@
-
-public class NumberUtils {
-    public static boolean isEven(int number) {
-        return number % 2 == 0;
-    }
-}
--- day5/src/SpaceReplacer.java
+++
@@ -1,9 +0,0 @@
-public class SpaceReplacer {
-    public static void main(String[] args) {
-        String text = "This  is   an    example with     multiple   spaces.";
-        String result = text.replaceAll("\\s+", " ");
-
-        System.out.println("Original: " + text);
-        System.out.println("Modified: " + result);
-    }
-}
